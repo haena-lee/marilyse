7de298da3ff53ed6cf89ba8693318b18bfc304d0
haena <32079807+haenabee@users.noreply.github.com>
2018-08-14 19:12:39 -0700

    Added portfolio structure and images

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    window.sr = ScrollReveal();
+    sr.reveal('.image', { 
+        origin: 'right', 
+        duration: 1000 
+      });
+      sr.reveal('.image', { 
+        origin: 'right', 
+        duration: 500 
+      });
+      sr.reveal('.image', { 
+        origin: 'right', 
+        duration: 1000 
+      });
+    }
+)
